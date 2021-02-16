5598bdc8b1733e068c87aeb620d08d3caebec023
Degrijo <degrijoyarik@gmail.com>
2021-02-16 17:59:14 +0300

    delete useless django views

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,25 +1,16 @@
 import debug_toolbar
-
 from django.urls import path, include
 from django.contrib import admin
-
 from rest_framework import routers
-
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from app.core.drf_views import AuthorizationViewSet, PlayerViewSet, RoomViewSet
-from app.core.django_views import statistic, home, algorithm, links, realization
 
 
 urlpatterns = [
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
-    path('statistic/', statistic),
-    path('', home),
-    path('algorithm/', algorithm),
-    path('links/', links),
-    path('realization/', realization),
 ]
 
 router = routers.SimpleRouter()
